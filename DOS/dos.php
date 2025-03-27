6dc7059215e196fc80f791b44771fce0a793a445
sirisolasveo <sirisolasveo@gmail.com>
2025-03-27 17:27:36 -0500

    primero

---
+++ DOS/dos.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página Web</title>
+</head>
+<body>
+    <h1>Bienvenidos a mi sitio web DOS</h1>
+    
+    <nav>
+        <a href="../index.php">Inicio</a>
+    </nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página Web</title>
+</head>
+<body>
+    <h1>Bienvenidos a mi sitio web</h1>
+    
+    <nav>
+        <a href="extra.php">extra</a>
+        <a href="UNO/uno.php">uno</a>
+        <a href="DOS/dos.php">dos</a>
+    </nav>
+</body>
+</html>
